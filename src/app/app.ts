8e6c7a0d524bbfe91de1c53e1526eea7bbf2782d
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-07-03 20:56:54 -0400

    Add OnInit implementation with interval subscription and cleanup logic

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,20 @@ import { Component, signal } from '@angular/core';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  private destroyRef = inject(DestroyRef);
 
+  ngOnInit(): void {
+    const subscription = interval(1000).subscribe((value) => {
+      console.log('Hello World ' + value);
+    });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+
+    // interval(1000).subscribe({
+    //   next: (value) => console.log('Hello World. ' + value),
+    //   error: (err) => console.log('Error', err),
+    //   complete: () => console.log('Complete')
+    // });
+  }
 }
